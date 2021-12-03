8d790378b9c74664880636e96713b623753c9c5a
saman <saman.arif@veeva.com>
2021-12-03 11:05:35 -0500

    Moving class to library

---
+++ src/library/Logging.ts
@@ -0,0 +1,41 @@
+class Logging {
+    private namespace = 'Default';
+
+    constructor(namespace?: string) {
+        if (namespace) this.namespace = namespace;
+    }
+
+    public info = (message: any) => {
+        if (typeof message === 'string') {
+            console.log(`[${this.getDate()}] [${this.namespace}] [INFO] ${message}`);
+        } else {
+            console.log(`[${this.getDate()}] [${this.namespace}] [INFO]`, message);
+        }
+    };
+
+    public warn = (message: any) => {
+        if (typeof message === 'string') {
+            console.log(`[${this.getDate()}] [${this.namespace}] [WARN] ${message}`);
+        } else {
+            console.log(`[${this.getDate()}] [${this.namespace}] [WARN]`, message);
+        }
+    };
+
+    public error = (message: any) => {
+        if (typeof message === 'string') {
+            console.log(`[${this.getDate()}] [${this.namespace}] [ERROR] ${message}`);
+        } else {
+            console.log(`[${this.getDate()}] [${this.namespace}] [ERROR]`, message);
+        }
+    };
+
+    public getDate = () => {
+        return new Date().toISOString();
+    };
+
+    public setNamespace = (namespace: string) => {
+        this.namespace = namespace;
+    };
+}
+
+export default Logging;
