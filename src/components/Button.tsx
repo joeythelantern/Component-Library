318a784451e56f7da9192e8252b2e67d693c5cd3
Nerdy Canuck <15827947+joeythelantern@users.noreply.github.com>
2021-12-19 15:39:06 -0500

    Changing button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,21 +3,19 @@ import React from 'react';
 export interface IButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     backgroundColor?: string;
     color?: string;
-    style?: React.CSSProperties;
 }
 
 export const Button: React.FunctionComponent<IButtonProps> = (props) => {
-    const { children, backgroundColor, style } = props;
+    const { children, backgroundColor, color, style } = props;
+
+    let _style = style;
+
+    /** Override Defaults */
+    if (backgroundColor && _style) _style.backgroundColor = backgroundColor;
+    if (color && _style) _style.color = color;
 
     return (
-        <button
-            style={{
-                ...style,
-                backgroundColor: backgroundColor || 'black',
-                color: 'white'
-            }}
-            {...props}
-        >
+        <button style={_style} {...props}>
             {children}
         </button>
     );
